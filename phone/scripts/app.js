69818d19c9834965b518f7db47abf0b26ceafcd6
NitinGupta9935 <guptanitin493@gmail.com>
2024-04-23 01:03:04 +0530

    Fixed No audio when answering an incoming call

--- phone/scripts/app.js
+++ phone/scripts/app.js
@@ -459,7 +459,7 @@ $(document).ready(function() {
                         stream      : ctxSip.Stream,
                         constraints : { audio : true, video : false },
                         render      : {
-                            remote : { audio: $('#audioRemote').get()[0] }
+                            remote : $('#audioRemote').get()[0]
                         },
                         RTCConstraints : { "optional": [{ 'DtlsSrtpKeyAgreement': 'true'} ]}
                     }
